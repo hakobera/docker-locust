7833cd0b43cce6617310d27a142fe4437f87bf7e
lifto <lifto@mac.com>
2016-09-15 16:18:23 -0400

    environment variable LOCUST_CLASSES allows you to specify user classes

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 LOCUST_CMD="/usr/local/bin/locust"
 LOCUST_OPTS="-f $SCENARIO_FILE --host=$TARGET_URL"
+
+# locust -f locust_file.py WebUserLocust MobileUserLocust
+# That's how you specify multiple User types.
+if [ "$LOCUST_CLASSES" ]; then
+    LOCUST_OPTS="$LOCUST_OPTS $LOCUST_CLASSES"
+fi
+
 LOCUST_MODE=${LOCUST_MODE:-standalone}
 
 if [ "$LOCUST_MODE" = "master" ]; then
